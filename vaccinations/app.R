a11b332776a383e9d9769784c3b32044807507d1
tenAlf <anko@LE-BO2D9.fritz.box>
2021-10-29 12:09:46 +0200

    WIP

--- vaccinations/app.R
+++ vaccinations/app.R
@@ -8,30 +8,57 @@
 #
 
 library(shiny)
+library(readr)
+library(dplyr)
+
+# Load Data ---------------------------------------------------------------
+
+data_url <- 
+    paste0("https://raw.githubusercontent.com",
+           "/robert-koch-institut/COVID-19-Impfungen_in_Deutschland",
+           "/master/Aktuell_Deutschland_Landkreise_COVID-19-Impfungen.csv")
+
+vac_data <-
+    read_csv(
+        data_url,
+        col_types = cols(
+            "Impfdatum"           = col_date(),
+            "LandkreisId_Impfort" = col_factor(),
+            "Altersgruppe"        = col_factor(),
+            "Impfschutz"          = col_factor(),
+            "Anzahl"              = col_integer()
+        )
+    )
+
 
 # Define UI for application that draws a histogram
 ui <- fluidPage(
 
     # Application title
-    titlePanel("Old Faithful Geyser Data"),
+    titlePanel("Verabreichte Impfungen"),
 
     # Sidebar with a slider input for number of bins 
     sidebarLayout(
         sidebarPanel(
-            sliderInput("bins",
-                        "Number of bins:",
-                        min = 1,
-                        max = 50,
-                        value = 30)
+            selectInput(inputId = "county_id", 
+                        label   = strong("Impfort (Landkreis)"),
+                        choices = unique(vac_data$LandkreisId_Impfort)),
+            
+            checkboxGroupInput("vac_group", 
+                               label = strong("Impfkategorie"),
+                               choices = list(
+                                   "Erstimpfung" = 1,
+                                   "Zweitimpfung" = 2,
+                                   "Drittimpfung" = 3)),
+            checkboxGroupInput("age_group",
+                               label = "Altersgruppen",
+                               choices = unique(vac_data$Altersgruppe))
         ),
-
-        # Show a plot of the generated distribution
         mainPanel(
-           plotOutput("distPlot")
-        )
+            plotOutput("distPlot")
     )
 )
-
+)
 # Define server logic required to draw a histogram
 server <- function(input, output) {
 
